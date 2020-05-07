f207c4854d0a82b439b7fd847d2174d747df0e77
Qian <zhangqiandut@gmail.com>
2020-05-06 21:42:56 -0400

    search suggestions system

---
+++ problems/1268.Search Suggestions System.py
@@ -0,0 +1,12 @@
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        ans = []
+        products.sort()
+        for i in range(1,len(searchWord)+1):
+            t=[]
+            for j in products:
+                products = list(filter(lambda x: x.startswith(searchWord[:i]), products))
+            ans.append(products[:3])
+       
+        return ans
+        
