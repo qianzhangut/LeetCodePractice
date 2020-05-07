71581ef23c4e3483bfbe2f71860e53b1a1406761
Qian <zhangqiandut@gmail.com>
2020-05-06 23:57:47 -0400

    count and say

---
+++ problems/038.Count and Say.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n==1:
+            return '1'
+        s = self.countAndSay(n-1)+'*'
+        ans, cnt = '', 1
+        for i in range(len(s)-1):
+            if s[i] == s[i+1]:
+                cnt += 1
+            else:
+                ans += str(cnt)+str(s[i])
+                cnt = 1
+        return ans
+        
