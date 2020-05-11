64a2a1ab049495185a38c7ab43e191eb1d9235ea
Qian <zhangqiandut@gmail.com>
2020-05-10 21:54:09 -0400

    combine two tables

---
+++ problems/175.Combine Two Tables.py
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT Person.FirstName, Person.LastName, Address.City, Address.State 
+from Person 
+LEFT JOIN Address 
+ON Person.PersonId = Address.PersonId ;
