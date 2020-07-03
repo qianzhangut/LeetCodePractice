58be7173b61326360befc54fa47f74cc489c0f87
Qian <zhangqiandut@gmail.com>
2020-07-03 12:10:19 -0400

    reverse string

---
+++ problems/344.Reverse String.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s[:] = [s[len(s) - i - 1] for i in range(len(s))]
