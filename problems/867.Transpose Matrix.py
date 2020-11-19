2e07370e3fb57abbea39aaf236f66c2ef87e926f
Qian <zhangqiandut@gmail.com>
2020-11-18 23:28:22 -0500

    another solution 867

--- problems/867.Transpose Matrix.py
+++ problems/867.Transpose Matrix.py
@@ -2,3 +2,8 @@ class Solution:
     def transpose(self, A: List[List[int]]) -> List[List[int]]:
               
         return [ [j[i] for j in A] for i in range(len(A[0]))]
+        
+class Solution:
+    def transpose(self, A: List[List[int]]) -> List[List[int]]:
+              
+        return zip(*A)
