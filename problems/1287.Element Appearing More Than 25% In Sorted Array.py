4b4265d5e75a882a1be9dd06daf4149a45449167
Qian <zhangqiandut@gmail.com>
2020-11-19 22:16:01 -0500

    question 1287 binary search

--- problems/1287.Element Appearing More Than 25% In Sorted Array.py
+++ problems/1287.Element Appearing More Than 25% In Sorted Array.py
@@ -8,3 +8,23 @@ class Solution:
                 d[item] += 1
             if d[item]/len(arr)>0.25:
                 return item
+                
+                
+# Binary search
+
+def findIndexOfFirst(x):
+    l = 0
+    r = len(arr) - 1
+    while l <= r:
+        mid = (l + r) // 2
+        if arr[mid] < x:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return l
+
+for q in range(1, 4):
+    idx = int(q * len(arr) / 4)
+    start = findIndexOfFirst(arr[idx])
+    if arr[start] == arr[start + len(arr)//4]:  
+        return arr[start]
