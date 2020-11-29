4cc0c4bcba296a8c7c39bac06e8837a6c99792ba
Qian <zhangqiandut@gmail.com>
2020-11-28 22:33:18 -0500

    reshape the matrix v2 slolution

--- problems/566.Reshape the Matrix.py
+++ problems/566.Reshape the Matrix.py
@@ -11,3 +11,34 @@ class Solution:
         for i in range(0, len(new), c):
             res.append(new[i:i+c]) 
         return res
+
+### brutal force
+
+
+class Solution:
+    def matrixReshape(self, nums: List[List[int]], r: int, c: int) -> List[List[int]]:
+        
+        
+        rows = len(nums)
+        cols = len(nums[0])
+        
+        
+        if r*c == rows*cols: #pass only the dimensions match
+            d = [[0]*c for i in range(r)]
+        
+            row_nums = 0
+            col_nums = 0
+            #simply Traverse through the nums array
+            
+			for i in range(0,rows):
+                for j in range(0,cols):
+                    d[row_nums][col_nums] = nums[i][j]
+                    col_nums +=1
+                    if col_nums == c: #traverse until a column is complete in a row
+                        row_nums +=1 
+                        col_nums = 0 #restart the column for new row
+                        
+            return d
+ 
+        else:
+            return nums
