887e87bdd475cbbcd2a457de9ac5344a7413fc87
Qian <zhangqiandut@gmail.com>
2020-12-03 21:12:18 -0500

    missing number second solution

--- problems/268.Missing Number.py
+++ problems/268.Missing Number.py
@@ -10,4 +10,9 @@ class Solution:
 		
 ## use formula 1 + 2 + 3 + ... + n = n*(n + 1)/2
 
-            
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        total = len(nums)*(len(nums)+1)//2
+        for item in nums:
+            total -= item
+        return total            
