c4b16c7af176feee4ab7d1596bd5a1a8f1906ad0
Qian <zhangqiandut@gmail.com>
2020-12-07 23:13:51 -0500

    1009 list comprehension

--- problems/1009.Complement of Base 10 Integer.py
+++ problems/1009.Complement of Base 10 Integer.py
@@ -10,3 +10,11 @@ class Solution:
             else:
                 res += '0'
         return int(res, 2)
+        
+
+## list comprehension
+class Solution:
+    def bitwiseComplement(self, N: int) -> int:
+        return int(''.join(['0' if i=='1' else '1' for i in bin(N)[2:]]), 2)
+        
+        
