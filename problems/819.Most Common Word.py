bc9ee74ad0d5b875e8856643095a54e1297c6856
Qian <zhangqiandut@gmail.com>
2020-12-09 22:20:02 -0500

    update most common word

--- problems/819.Most Common Word.py
+++ problems/819.Most Common Word.py
@@ -1,3 +1,5 @@
+
+## use regex
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         import re
@@ -19,3 +21,24 @@ class Solution:
         
 
         return res
+        
+        
+## use isalnum
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        par = "".join([c.lower() if c.isalnum() else ' ' for c in paragraph]).split()
+        par = [i for i in par if i not in banned]
+        d = {}
+        max = 0
+        res = ''
+        for i in par:
+            if i not in d:
+                d[i] = 1 
+            else:
+                d[i] += 1
+            if d[i]>max:
+                max = d[i]
+                res = i
+        
+
+        return res 
