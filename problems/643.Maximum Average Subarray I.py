300544d334df9735948ed8def23e1b3253fcb33e
Qian <zhangqiandut@gmail.com>
2020-12-16 22:47:07 -0500

    max average subarry v2

--- problems/643.Maximum Average Subarray I.py
+++ problems/643.Maximum Average Subarray I.py
@@ -1,10 +1,21 @@
+## sliding window
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        best = now = sum(nums[:k])
+        res = now = sum(nums[:k])
         for i in range(k,len(nums)):
             now += nums[i] - nums[i-k]
-            if now>best:
-                best = now
-        return best/k
+            if now>res:
+                res = now
+        return res/k
 
+## cumulative sum
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        n = nums
         
+        for i in range(1, len(nums)):
+            n[i] = n[i-1] + nums[i]
+        res = n[k-1]
+        for i in range(k, len(n)):
+            res = max(res, n[i]-n[i-k])
+        return res/k
