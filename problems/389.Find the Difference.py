cd2278bd08efaebcc5ae46140c463c1676e71374
Qian <zhangqiandut@gmail.com>
2020-12-18 23:11:22 -0500

    use xor

--- problems/389.Find the Difference.py
+++ problems/389.Find the Difference.py
@@ -1,6 +1,19 @@
+## count
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         for i in t:
             if(t.count(i)!=s.count(i)):
                 return i
         return ""
+        
+        
+## use xor
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        code = 0
+        for ch in s + t:
+            code ^= ord(ch)
+        return chr(code)
+        
+    
