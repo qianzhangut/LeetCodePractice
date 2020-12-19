2e326b02162cf3a392cef4edb8a6f1cf46c9964f
Qian <zhangqiandut@gmail.com>
2020-12-18 23:15:23 -0500

    use dictionary

--- problems/389.Find the Difference.py
+++ problems/389.Find the Difference.py
@@ -16,4 +16,18 @@ class Solution:
             code ^= ord(ch)
         return chr(code)
         
-    
+
+
+## use dictionary
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        dic = {}
+        for ch in s:
+            dic[ch] = dic.get(ch, 0) + 1
+        for ch in t:
+            if dic.get(ch, 0) == 0:
+                return ch
+            else:
+                dic[ch] -= 1
+                
+## use ord diff
