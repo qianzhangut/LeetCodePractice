059c4a1b3abf57968aa5b0ad32cb3feea66b42fd
Qian <zhangqiandut@gmail.com>
2020-12-21 22:40:08 -0500

    Fibonacci Number v2

--- problems/509.Fibonacci Number.py
+++ problems/509.Fibonacci Number.py
@@ -5,3 +5,19 @@ class Solution:
         for i in range(2, n+1):
             res[i] = res[i-1] + res[i-2]
         return res[n]
+        
+        
+## Storage reduced to O(1)
+class Solution:
+    def fib(self, n: int) -> int:
+        if n<=1:
+            return n
+        if n == 2:
+            return 1
+        
+        p1, p2, now = 1, 1 , 0
+        for i in range(3, n+1):
+            now = p1 + p2
+            p2 = p1
+            p1 = now
+        return now
