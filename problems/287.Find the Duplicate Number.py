0026ddd1a0efc5ea64c4201f306976af32b0cf3e
Qian <zhangqiandut@gmail.com>
2020-12-25 14:03:18 -0500

    find the duplicate number ver2

--- problems/287.Find the Duplicate Number.py
+++ problems/287.Find the Duplicate Number.py
@@ -8,3 +8,13 @@ class Solution:
             if d[num]>1:
                 return num
         
+        
+ ## use dictionary count
+ 
+ class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        d = {}
+        for num in nums:
+            d[num] = d.get(num,0) + 1
+            if d[num] > 1:
+                return num
