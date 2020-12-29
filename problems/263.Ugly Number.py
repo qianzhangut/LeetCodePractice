f7f0db82661d8f4cf8fe2e096ca6eac87cea025e
Qian <zhangqiandut@gmail.com>
2020-12-28 21:28:31 -0500

    ugly number

---
+++ problems/263.Ugly Number.py
@@ -0,0 +1,17 @@
+## check if multiple of 2,3,5, if yes divided by 2, 3, 5, else return false
+
+class Solution:
+    def isUgly(self, num: int) -> bool:
+        while num>1:
+            if num%2 == 0:
+                num = num // 2
+            elif num%3 == 0:
+                num //= 3
+            elif num%5 == 0:
+                num //= 5
+            else:
+                return False
+                break
+        return num == 1
+    
+                
