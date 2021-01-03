127de25eee1ceb67c62b84de602ff9b1b0acf7a0
Qian <zhangqiandut@gmail.com>
2021-01-03 16:02:48 -0500

    power of two

--- problems/231.Power of Two.py
+++ problems/231.Power of Two.py
@@ -1,7 +1,12 @@
-
-
+## didived by 2
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         while n and n%2 == 0:
             n //= 2
         return n == 1
+        
+
+## use trick n & (n-1) 
+ class Solution:
+    def isPowerOfFour(self, num):
+        return num > 0 and num & (num-1) == 0 
