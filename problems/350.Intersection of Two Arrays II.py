9d8159c329bc2906dcd8ed185ee95555cc4c7883
Qian <zhangqiandut@gmail.com>
2021-01-03 19:56:11 -0500

    intersection of two arrays ver2

--- problems/350.Intersection of Two Arrays II.py
+++ problems/350.Intersection of Two Arrays II.py
@@ -13,3 +13,20 @@ class Solution:
                     nums1.remove(j)
                     res.append(j)
         return res
+        
+## use dictionary
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        cnt, res = {}, []
+        
+        for i in nums1:
+            if i not in cnt:
+                cnt[i] = 1
+            else:
+                cnt[i] += 1
+            
+        for i in nums2:
+            if i in nums1 and cnt[i]>0:
+                res.append(i)
+                cnt[i] -= 1
+        return res
