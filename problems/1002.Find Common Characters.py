e8cf0e8b7cc32bb03487b83d90360bcb2b02c16e
Qian <zhangqiandut@gmail.com>
2021-01-03 20:21:50 -0500

    find common characters ver 2

--- problems/1002.Find Common Characters.py
+++ problems/1002.Find Common Characters.py
@@ -1,3 +1,19 @@
+## brutal force
+class Solution:
+    def commonChars(self, A: List[str]) -> List[str]:
+        old = list(A[0])
+        
+        for word in A[1:]:
+            new = []
+            for c in word:
+                if c in old:
+                    new.append(c)
+                    old.remove(c)
+            old = new
+            
+        return old
+
+
 ## use counter
 
 class Solution:
@@ -10,3 +26,6 @@ class Solution:
             res &= collections.Counter(a)
             
         return list(res.elements())
+        
+        
+        
