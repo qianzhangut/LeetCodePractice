a322831d5e69c6f681b104becd8c1c76f2bce6b1
Qian <zhangqiandut@gmail.com>
2021-01-09 23:58:07 -0500

    binary number with alternating bits xor

--- problems/693.Binary Number with Alternating Bits.py
+++ problems/693.Binary Number with Alternating Bits.py
@@ -8,5 +8,16 @@ class Solution:
 class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
         return (n&(n>> 1)) ==0 and  (n & (n >> 2)) == (n >> 2)
+
+## bit operation xor
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        if not n:
+            return False
+        num = n ^ (n >> 1)
+        
+        return all(i=='1' for i in bin(num)[2:])
+        
+        
         
         
