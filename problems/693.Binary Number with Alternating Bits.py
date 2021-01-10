d0e2539b5ffc6ed77f0542828b81a78820271047
Qian <zhangqiandut@gmail.com>
2021-01-09 23:53:14 -0500

    binary number with alternating bits operation

--- problems/693.Binary Number with Alternating Bits.py
+++ problems/693.Binary Number with Alternating Bits.py
@@ -3,5 +3,10 @@ class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
         b = bin(n)[2:]
         return all(b[i] != b[i+1] for i in range(len(b)-1))
+
+## bit operation       
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        return (n&(n>> 1)) ==0 and  (n & (n >> 2)) == (n >> 2)
         
         
