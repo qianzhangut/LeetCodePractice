5acb1bd73c5e73b3f648b62e6597a79d1eecd795
Qian <zhangqiandut@gmail.com>
2021-01-11 22:50:41 -0500

    to lower case ver2

--- problems/709.To Lower Case.py
+++ problems/709.To Lower Case.py
@@ -2,3 +2,8 @@
 class Solution:
     def toLowerCase(self, str: str) -> str:
         return str.lower()
+        
+## use ord and chr
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return "".join([chr(ord(i)+32) if ord("A")<=ord(i)<=ord("Z") else i for i in str ])
