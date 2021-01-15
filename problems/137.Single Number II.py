2dbb9222970fbe847ab2b3f5361940afe33c70c2
Qian <zhangqiandut@gmail.com>
2021-01-14 21:08:56 -0500

    single number ver2

--- problems/137.Single Number II.py
+++ problems/137.Single Number II.py
@@ -11,3 +11,11 @@ class Solution:
         for i in d:
             if d[i] == 1:
                 return i
+                
+## use count
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        for i in nums:
+            if nums.count(i) == 1:
+                return i
+            
