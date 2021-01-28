430bcbff346d911458dabd7e8f6b7298bd1f1a0c
Qian <zhangqiandut@gmail.com>
2021-01-27 21:59:32 -0500

    first bad version

---
+++ problems/278.First Bad Version.py
@@ -0,0 +1,22 @@
+## binary search
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l, r = 1, n
+        while l<r:
+            m = l + (r-l) // 2
+            
+            if isBadVersion(m):
+                r = m 
+            else:
+                l = m+1
+        return l
