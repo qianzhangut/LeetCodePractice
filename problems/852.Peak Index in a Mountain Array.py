b97d20dffcb81ac78a795810d67c9cf7a59e41a2
Qian <zhangqiandut@gmail.com>
2021-01-28 22:28:19 -0500

    peak index in a mountain array

---
+++ problems/852.Peak Index in a Mountain Array.py
@@ -0,0 +1,14 @@
+## binary search
+
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        l, r = 1, len(arr)-1
+        while l<r:
+            m = l + (r-l)//2
+            if arr[m-1]<arr[m]>arr[m+1]:
+                return m
+            elif arr[m-1]>arr[m]>arr[m+1]:
+                r = m
+            else:
+                l = l+1
+        return l
