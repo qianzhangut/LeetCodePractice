1fa9d0762d1dcca0c8e67490d7d43db4643eea02
Qian <zhangqiandut@gmail.com>
2021-02-04 23:42:13 -0500

    441 arranging coins sum of arithmetic sequence

--- problems/441.Arranging Coins.py
+++ problems/441.Arranging Coins.py
@@ -24,3 +24,8 @@ class Solution:
                 l = m + 1
                 
         return r
+        
+ ## sum of arithmetic sequences 
+ class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        return int((2*n+0.25)**0.5-0.5)
