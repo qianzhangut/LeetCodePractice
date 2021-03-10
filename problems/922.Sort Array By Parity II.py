5c4bcf5b4b0a82dec68a96fbe7f903365ce01096
Qian <zhangqiandut@gmail.com>
2021-03-09 23:05:24 -0500

    sort array by parity 2 ver3

--- problems/922.Sort Array By Parity II.py
+++ problems/922.Sort Array By Parity II.py
@@ -25,3 +25,18 @@ class Solution:
         
         return ans
         
+## o(1) space solution
+
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        i, j = 0, 1
+        n = len(nums)
+        
+        while i<n and j <n:
+            if nums[i]%2 == 0:
+                i += 2
+            elif nums[j]%2 == 1:
+                j += 2
+            else:
+                nums[i], nums[j] = nums[j], nums[i]
+        return nums
