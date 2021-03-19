7524943d202f42401c681522f047ce88db39439f
Qian <zhangqiandut@gmail.com>
2021-03-18 23:44:01 -0400

    binary prefix divisible by 5 bit op

--- problems/1018.Binary Prefix Divisible By 5.py
+++ problems/1018.Binary Prefix Divisible By 5.py
@@ -3,3 +3,12 @@ class Solution:
         for i in range(1, len(A)):
             A[i] += A[i - 1] * 2
         return [x % 5 == 0 for x in A]
+        
+# bit operation
+class Solution:
+    def prefixesDivBy5(self, A: List[int]) -> List[bool]:
+        ans, b = [], 0
+        for a in A:
+            b = b << 1 | a
+            ans.append(b % 5 == 0)
+        return ans
