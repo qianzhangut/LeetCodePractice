ad17ce117d6e589193b5b30bf30e9c23656478a3
Qian <zhangqiandut@gmail.com>
2021-03-23 23:40:35 -0400

    robot return to origin ver2

--- problems/657.Robot Return to Origin.py
+++ problems/657.Robot Return to Origin.py
@@ -5,3 +5,18 @@ class Solution:
         for i in moves:
             cnt[i] = cnt.get(i,0)+1
         return cnt['L'] == cnt['R'] and cnt['U'] == cnt['D']
+        
+        
+#use coordinate
+
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x = y = 0
+        for move in moves:
+            if move == 'U': y -= 1
+            elif move == 'D': y += 1
+            elif move == 'L': x -= 1
+            elif move == 'R': x += 1
+
+        return x == y == 0
+ 
