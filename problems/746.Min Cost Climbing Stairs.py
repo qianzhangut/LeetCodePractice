779d58d7e2c10c0a1e7d12c22ca99959a045c568
Qian <zhangqiandut@gmail.com>
2021-03-29 23:26:57 -0400

    min cost climbing stairs ver 2

--- problems/746.Min Cost Climbing Stairs.py
+++ problems/746.Min Cost Climbing Stairs.py
@@ -8,3 +8,15 @@ class Solution:
             
             
         return min(dp[-1], dp[-2])
+        
+        
+# O(1) space solution
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        f1, f2 = cost[0], cost[1]
+        
+        for i in range(2, len(cost)):
+            f1, f2 = f2, cost[i] + min(f1, f2)
+        
+        return min(f1, f2)
