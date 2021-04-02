55680a8512d43f2b577cfa195b1b088742d0fb0c
Qian <zhangqiandut@gmail.com>
2021-04-01 22:33:03 -0400

    monotonic array ver2

--- problems/896.Monotonic Array.py
+++ problems/896.Monotonic Array.py
@@ -2,3 +2,12 @@
 class Solution:
     def isMonotonic(self, A: List[int]) -> bool:
         return sorted(A) == A or sorted(A) == A[::-1]
+        
+## O(n)
+class Solution:
+    def isMonotonic(self, A: List[int]) -> bool:
+        dif = []
+        for i in range(1, len(A)):
+            dif.append(A[i]-A[i-1])
+            
+        return all(x>= 0 for x in dif) or all(x<= 0 for x in dif)
