6e436293e8b83a604a0c214eb428ee27735ee61f
Qian <zhangqiandut@gmail.com>
2021-05-04 23:58:25 -0400

    rotated digits

---
+++ problems/788.Rotated Digits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rotatedDigits(self, N: int) -> int:
+        s1 = set([1, 8, 0])
+        s2 = set([1, 8, 0, 6, 9, 2, 5])
+        def isGood(x):
+            s = set([int(i) for i in str(x)])
+            return s.issubset(s2) and not s.issubset(s1)
+        return sum(isGood(i) for i in range(N + 1))
