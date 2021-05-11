94c109d021a5353a2be9c3bb9ecfd8f9be19d36a
Qian <zhangqiandut@gmail.com>
2021-05-10 23:58:31 -0400

    positions of large group

---
+++ problems/830.Positions of Large Groups.py
@@ -0,0 +1,9 @@
+class Solution:
+    def largeGroupPositions(self, s: str) -> List[List[int]]:
+        i, j, N = 0, 0, len(s)
+        res = []
+        while i < N:
+            while j < N and s[j] == s[i]: j += 1
+            if j - i >= 3: res.append([i, j - 1])
+            i = j
+        return res
