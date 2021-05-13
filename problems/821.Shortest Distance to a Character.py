e24dc2875f466fa661d4939cfcb9f6cb8abf21bb
Qian <zhangqiandut@gmail.com>
2021-05-12 23:02:44 -0400

    shortest distance dp

--- problems/821.Shortest Distance to a Character.py
+++ problems/821.Shortest Distance to a Character.py
@@ -7,3 +7,19 @@ class Solution:
                 pos = i
             ans[i] = min(ans[i], abs(i-pos))
         return ans
+        
+        
+        
+# DP
+class Solution:
+    def shortestToChar(self, s: str, c: str) -> List[int]:
+        n = len(s)
+        
+        ans = [0 if i == c else n for i in s]
+        
+        for i in range(1, n):
+            ans[i] = min(ans[i], ans[i-1] + 1)
+        for i in range(n-2, -1, -1):
+            ans[i] = min(ans[i], ans[i+1] + 1)
+            
+        return ans
