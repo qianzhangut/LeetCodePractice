817301088486fc551b16eb7bf073af2623c58052
Qian <zhangqiandut@gmail.com>
2021-06-04 00:04:12 -0400

    two sum iii

--- problems/167.Two Sum II - Input array is sorted.py
+++ problems/167.Two Sum II - Input array is sorted.py
@@ -1,3 +1,5 @@
+
+##hashmap
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         d = {}
@@ -8,3 +10,17 @@ class Solution:
             d[num] = ind
             
         return []
+        
+        
+## two pointer        
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+            l, r = 0, len(numbers)-1
+            while l < r:
+                s = numbers[l] + numbers[r]
+                if s == target:
+                    return [l+1, r+1]
+                elif s < target:
+                    l += 1
+                else:
+                    r -= 1
