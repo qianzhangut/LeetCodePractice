cc51f6a72ba62484e223434cac277e7a4e86041c
Qian <zhangqiandut@gmail.com>
2021-07-25 00:00:20 -0400

    longest palindroome v2

--- problems/409.Longest Palindrome.py
+++ problems/409.Longest Palindrome.py
@@ -6,3 +6,20 @@ class Solution:
             if ans % 2 == 0 and v % 2 == 1:
                 ans += 1
         return ans
+        
+        
+        
+        
+        
+########hash for odd numbers
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        hash = set()
+        
+        for c in s:
+            if c not in hash:
+                hash.add(c)
+            else:
+                hash.remove(c)
+
+        return len(s)-len(hash)+1 if len(hash)>0 else len(s) 
