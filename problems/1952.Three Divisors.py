a991a4117fa6ce6a37833aed2e0f9ca5e8a977db
Qian <zhangqiandut@gmail.com>
2021-08-02 23:22:04 -0400

    1952.Three Divisors.py update

--- problems/1952.Three Divisors.py
+++ problems/1952.Three Divisors.py
@@ -1,7 +1,9 @@
 class Solution:
     def isThree(self, n: int) -> bool:
         cnt = 0
-        for i in range(1, n+1):
+        for i in range(2, n):
             if n%i ==0:
                 cnt +=1
-        return cnt == 3
+                if cnt > 1:
+                    return False
+        return cnt == 1
