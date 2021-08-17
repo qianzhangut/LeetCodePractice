2106e8668b537407ab05f0650f9ec1f42e05d91c
Qian <zhangqiandut@gmail.com>
2021-08-16 23:40:36 -0400

    1880.Check if Word Equals Summation of Two Words.py

---
+++ problems/1880.Check if Word Equals Summation of Two Words.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+        first = sum([(ord(f)-ord('a'))*(10**(len(firstWord)-1-i)) for i,f in enumerate(firstWord)])
+        second =sum([(ord(f)-ord('a'))*(10**(len(secondWord)-1-i)) for i,f in enumerate(secondWord)])
+        target =sum([(ord(f)-ord('a'))*(10**(len(targetWord)-1-i)) for i,f in enumerate(targetWord)])
+        
+        return first + second == target
