0b44f89ab0aaca85e324a8e126c996505597c9a1
Qian <zhangqiandut@gmail.com>
2021-09-02 23:35:49 -0400

    1796.Second Largest Digit in a String.pyver2

--- problems/1796.Second Largest Digit in a String.py
+++ problems/1796.Second Largest Digit in a String.py
@@ -7,3 +7,18 @@ class Solution:
 
         res = sorted(list(set(l)))
         return res[-2] if len(res)>1 else -1
+        
+        
+ 
+## two pointers
+class Solution:
+    def secondHighest(self, s: str) -> int:
+        first = second = -1
+        for c in s:
+            if c.isdigit():
+                d = int(c)
+                if d > first:
+                    first, second = d, first
+                elif first > d > second:
+                    second = d
+        return second
