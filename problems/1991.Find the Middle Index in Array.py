90032cdfff13cdc82f2d981a8fa7a6fa722b2aed
Qian <zhangqiandut@gmail.com>
2021-09-15 23:21:27 -0400

    1991.Find the Middle Index in Array ver2

--- problems/1991.Find the Middle Index in Array.py
+++ problems/1991.Find the Middle Index in Array.py
@@ -5,3 +5,14 @@ class Solution:
                 return i
             
         return -1
+        
+        
+## pointer 
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        s, p_sum = sum(nums), 0
+        for i, num in enumerate(nums):
+            if p_sum == (s - num) / 2.0:
+                return i
+            p_sum += num
+        return -1
