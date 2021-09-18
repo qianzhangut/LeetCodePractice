6fc91c4ff8fecb550ab39c9ce36f8a0952e25087
Qian <zhangqiandut@gmail.com>
2021-09-17 23:57:12 -0400

    1844.Replace All Digits with Characters ver2

--- problems/1844.Replace All Digits with Characters.py
+++ problems/1844.Replace All Digits with Characters.py
@@ -9,3 +9,8 @@ class Solution:
 
         return res
             
+#one line           
+class Solution:
+    def replaceDigits(self, s: str) -> str:
+        return "".join([chr(ord(s[i-1]) + int(s[i])) if i%2 else s[i] for i in range(len(s))])
+            
