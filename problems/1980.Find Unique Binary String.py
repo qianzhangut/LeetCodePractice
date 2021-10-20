d87953d651ca656810df4cd73c23ca4a36686347
Qian <zhangqiandut@gmail.com>
2021-10-19 21:34:41 -0400

    1980.Find Unique Binary String.py ver2

--- problems/1980.Find Unique Binary String.py
+++ problems/1980.Find Unique Binary String.py
@@ -1,3 +1,9 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         return ''.join(['1' if num[i]=='0' else '0' for i,num in enumerate(nums)])
+
+
+## ver2
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        return ''.join([str(1-int(num[i])) for i,num in enumerate(nums)])
