b5dbb25edc7fb368a497219781680f213ac2e703
Qian <zhangqiandut@gmail.com>
2021-10-24 19:48:36 -0400

    2042.Check if Numbers Are Ascending in a Sentence.py ver 2

--- problems/2042.Check if Numbers Are Ascending in a Sentence.py
+++ problems/2042.Check if Numbers Are Ascending in a Sentence.py
@@ -3,4 +3,14 @@ class Solution:
         
         ans = [int(i) for i in s.split(" ") if i.isdigit()]
         return all(ans[j]<ans[j+1] for j in range(len(ans)-1))
+ #walnut expression 
             
+class Solution:
+    def areNumbersAscending(self, s: str) -> bool:
+        prev = 0
+        for token in s.split():
+            if token.isnumeric():                
+                if (curr := int(token)) <= prev:
+                    return False
+                prev = curr
+        return True
