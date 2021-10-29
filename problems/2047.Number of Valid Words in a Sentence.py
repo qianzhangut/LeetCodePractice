e9ee11f1cdfc9c167078cd6256de1aaf9943511d
Qian <zhangqiandut@gmail.com>
2021-10-28 22:23:59 -0400

    2047.Number of Valid Words in a Sentence.py regex

--- problems/2047.Number of Valid Words in a Sentence.py
+++ problems/2047.Number of Valid Words in a Sentence.py
@@ -11,3 +11,11 @@ class Solution:
             return True 
         
         return sum(fn(word) for word in sentence.split())
+        
+        
+        
+ ## regex
+ class Solution:
+    def countValidWords(self, sentence: str) -> int:
+        pattern = re.compile(r'(^[a-z]+(-[a-z]+)?)?[,.!]?$')
+        return sum(bool(pattern.match(word)) for word in sentence.split())
