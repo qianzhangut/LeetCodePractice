1ea8b363b9b8062d7f6a81d86d6016d402ce286f
Qian <zhangqiandut@gmail.com>
2021-11-11 23:31:14 -0500

    2016.Maximum Difference Between Increasing Elements.py ver2

--- problems/2016.Maximum Difference Between Increasing Elements.py
+++ problems/2016.Maximum Difference Between Increasing Elements.py
@@ -9,3 +9,14 @@ class Solution:
                 if i<j:
                     ans = max(ans, nums[j]-nums[i])
         return -1 if ans == 0 else ans
+        
+## best time and sell
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        
+        ans, min_pointer = 0, nums[0]
+        for i in range(len(nums)):
+            ans = max(ans, nums[i] - min_pointer)
+            min_pointer = min(min_pointer, nums[i])
+            
+        return -1 if ans == 0 else ans
