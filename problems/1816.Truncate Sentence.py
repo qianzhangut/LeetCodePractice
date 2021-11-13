bf58f0ece2d29480276cf0167d61c7ef1989504c
Qian <zhangqiandut@gmail.com>
2021-11-13 00:12:40 -0500

    1816.Truncate Sentence.py ver2

--- problems/1816.Truncate Sentence.py
+++ problems/1816.Truncate Sentence.py
@@ -1,3 +1,15 @@
 class Solution:
     def truncateSentence(self, s: str, k: int) -> str:
         return " ".join(s.split(" ")[:k])
+        
+        
+        
+#pointer
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        for i in range(len(s)):
+            if s[i] == " ":
+                k -= 1
+            if k == 0:
+                return s[:i]
+        return s
