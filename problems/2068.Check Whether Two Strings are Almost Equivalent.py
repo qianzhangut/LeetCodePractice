46b0ea3e1e830dff3a62b96127340e20bc2558de
Qian <zhangqiandut@gmail.com>
2021-11-15 22:16:16 -0500

    2068.Check Whether Two Strings are Almost Equivalent.pyver2

--- problems/2068.Check Whether Two Strings are Almost Equivalent.py
+++ problems/2068.Check Whether Two Strings are Almost Equivalent.py
@@ -9,3 +9,9 @@ class Solution:
             d[j] = d.get(j,0) -1
             
         return max([abs(k) for k in d.values()])<=3
+        
+## counter        
+class Solution:
+    def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
+    
+        return all(v < 4 for v in ((Counter(word1)-Counter(word2))+(Counter(word2)-Counter(word1))).values())
