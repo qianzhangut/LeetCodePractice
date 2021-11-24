6f2a541252aee1ecd547daf9e29631b1b0fbdf12
Qian <zhangqiandut@gmail.com>
2021-11-23 23:38:23 -0500

    2078.Two Furthest Houses With Different Colors.py ver2

--- problems/2078.Two Furthest Houses With Different Colors.py
+++ problems/2078.Two Furthest Houses With Different Colors.py
@@ -8,3 +8,17 @@ class Solution:
                 if colors[i] != colors[j]:
                     ans = max(ans, abs(i-j))
         return ans
+        
+        
+# O(n)
+
+class Solution:
+    def maxDistance(self, colors: List[int]) -> int:
+        ans = -1
+        for i in range(len(colors)):
+                if colors[i] != colors[0]:
+                    ans = max(ans, abs(i-0))
+        for j in range(len(colors)):
+                if colors[j] !=colors[-1]:
+                    ans = max(ans, abs(len(colors)-j-1))
+        return ans
