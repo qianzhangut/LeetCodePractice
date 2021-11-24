371f31e2762c11261e47fad8c0b2091fd53c5284
Qian <zhangqiandut@gmail.com>
2021-11-23 23:43:22 -0500

    first and last hourse will be the furthest house

--- problems/2078.Two Furthest Houses With Different Colors.py
+++ problems/2078.Two Furthest Houses With Different Colors.py
@@ -22,3 +22,12 @@ class Solution:
                 if colors[i] !=colors[-1]:
                     ans = max(ans, abs(len(colors)-i-1))
         return ans
+        
+        
+# The first and the last house can be one of the furthest houses
+class Solution:
+    def maxDistance(self, colors: List[int]) -> int:
+        i, j = 0, len(colors) - 1
+        while colors[0] == colors[j]: j -= 1
+        while colors[-1] == colors[i]: i += 1
+        return max(len(colors) - 1 - i, j)
