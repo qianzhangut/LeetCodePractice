82b9b54bd6e7f5549082e39ded9287468e8a0a39
Qian <zhangqiandut@gmail.com>
2021-12-09 00:10:03 -0500

    2089.Find Target Indices After Sorting Array.py ver2

--- problems/2089.Find Target Indices After Sorting Array.py
+++ problems/2089.Find Target Indices After Sorting Array.py
@@ -1,5 +1,23 @@
+#brutal force
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
         temp = sorted(nums)
         
         return [i for i, num in enumerate(temp) if num ==target]
+        
+        
+        
+ 
+ ## 2 pointers
+ 
+ class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        lt, eq = 0, 0
+        
+        for i in nums:
+            if i < target:
+                lt += 1
+            elif i == target:
+                eq += 1
+                
+        return list(range(lt, lt+eq))
