48e0624108d15ac8f10730f92694dfa3587bc24c
Qian <zhangqiandut@gmail.com>
2022-01-18 21:30:36 -0500

    2119.A Number After a Double Reversal.py ver 2

--- problems/2119.A Number After a Double Reversal.py
+++ problems/2119.A Number After a Double Reversal.py
@@ -1,3 +1,9 @@
 class Solution:
     def isSameAfterReversals(self, num: int) -> bool:
         return num == int(str(int(str(num)[::-1]))[::-1])
+        
+        
+##  0 or end digit is 0
+class Solution:
+    def isSameAfterReversals(self, num: int) -> bool:
+        return (num == 0) | (num % 10)
