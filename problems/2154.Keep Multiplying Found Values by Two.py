2fe26d34017201426f6d8b036df72c2c6a634cfc
Qian <zhangqiandut@gmail.com>
2022-02-02 23:05:39 -0500

    2154.Keep Multiplying Found Values by Two.py set

--- problems/2154.Keep Multiplying Found Values by Two.py
+++ problems/2154.Keep Multiplying Found Values by Two.py
@@ -11,3 +11,17 @@ class Solution:
                 i += 1
      
         return original
+        
+## use setclass Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        n = set(nums)
+        
+        i=0
+        while i<len(n):
+            if original in n:
+                original *= 2
+                i += 1
+            else:
+                i += 1
+     
+        return original
