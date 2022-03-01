bfa4ba2f007a9ef8556aee24ad73514bb625df9f
Qian <zhangqiandut@gmail.com>
2022-02-28 22:40:14 -0500

    2180.Count Integers With Even Digit Sum.py map func

--- problems/2180.Count Integers With Even Digit Sum.py
+++ problems/2180.Count Integers With Even Digit Sum.py
@@ -1,3 +1,4 @@
+##brutal force
 class Solution:
     def countEven(self, num: int) -> int:
         ans = 0
@@ -10,4 +11,13 @@ class Solution:
             if res%2 ==0:
                 ans += 1
                 
+        return ans
+        
+#map
+class Solution:
+    def countEven(self, num: int) -> int:
+        ans = 0
+        for i in range(1, num+1):
+            if sum(map(int, str(i))) %2 ==0 :
+                ans += 1
         return ans
