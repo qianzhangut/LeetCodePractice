d43e9e5c0efd1d2ef4a9282bcbd718f4a432baa6
Qian <zhangqiandut@gmail.com>
2022-03-08 22:34:26 -0500

    2194.Cells in a Range on an Excel Sheet.py list compre

--- problems/2194.Cells in a Range on an Excel Sheet.py
+++ problems/2194.Cells in a Range on an Excel Sheet.py
@@ -1,8 +1,6 @@
 class Solution:
     def cellsInRange(self, s: str) -> List[str]:
-        ans = []
+        
         a1, a2, r1, r2 = s[0], s[3], int(s[1]), int(s[-1])
-        for i in range(ord(a1), ord(a2)+1):
-            ans += [chr(i)+str(j) for j in range(r1,r2+1)]
-            
-        return ans
+        
+        return [chr(i)+str(j)  for i in range(ord(a1), ord(a2)+1) for j in range(r1,r2+1)]
