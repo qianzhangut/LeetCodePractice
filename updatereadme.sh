389d2c3df05745a05fa8b9c2eb6c55f74944afde
qianzhangut <zhangqiandut@gmail.com>
2022-08-16 22:23:28 -0400

    update commit msg

--- updatereadme.sh
+++ updatereadme.sh
@@ -5,12 +5,14 @@ cd ${REPO_DIR}
 python3 generatelist.py
 
 
-message="update Readme file on $(date)"
+message1="update Readme file on $(date)"
+message2="update list file on $(date)"
 GIT=`which git`
 
-
-${GIT} add list README.md
-${GIT} commit -m "$message"
+${GIT} add list
+${GIT} commit -m "$message2"
+${GIT} add README.md
+${GIT} commit -m "$message1"
 
 gitPush=$(${GIT} push origin master)
 echo "$gitPush"
