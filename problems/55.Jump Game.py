8f73c0a7d0e4cb418639818f64b9e56f71eb7cc6
qianzhangut <zhangqiandut@gmail.com>
2022-10-31 23:12:28 -0400

    55.Jump Game.py

--- problems/55.Jump Game.py
+++ problems/55.Jump Game.py
@@ -9,3 +9,23 @@ class Solution:
                 return False;
                     
         return False
+        
+ ##Forward
+ class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        dis = 0
+        for j in range( len(nums)):
+            if  j>dis:
+                return False
+            dis = max(dis, j+nums[j])
+                    
+        return True
+        
+ #backward      
+ class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        dis = len(nums)-1
+        for j in range( len(nums))[::-1]:
+            if  j + nums[j] >=dis:
+                dis = j
+        return not dis
