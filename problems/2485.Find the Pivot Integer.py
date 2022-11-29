373ee1e18bd4ca1647f7d2720350f4ceb179751b
qianzhangut <zhangqiandut@gmail.com>
2022-11-28 22:44:25 -0500

    2485.Find the Pivot Integer.py ver2

--- problems/2485.Find the Pivot Integer.py
+++ problems/2485.Find the Pivot Integer.py
@@ -5,3 +5,10 @@ class Solution:
             if sum(res[:i+1]) == sum(res[i:]):
                 return i+1
         return -1
+        
+##x*x = sum
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        nsum = n*(n+1)/2
+        x = int(sqrt(nsum))
+        return x if x*x==nsum else -1
