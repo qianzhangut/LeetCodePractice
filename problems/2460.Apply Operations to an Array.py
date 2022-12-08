25e05b96b3546c629fb6cf5af33d4065568d74cc
qianzhangut <zhangqiandut@gmail.com>
2022-12-07 22:57:56 -0500

    2460.Apply Operations to an Array.py

---
+++ problems/2460.Apply Operations to an Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                nums[i] *= 2
+                nums[i+1] = 0
+
+        return [j for j in nums if j] + [k for k in nums if not k]
