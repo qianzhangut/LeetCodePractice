54be789145dbc95858611877ef3a8028c4015532
qianzhangut <zhangqiandut@gmail.com>
2022-12-12 22:07:39 -0500

    2486.Append Characters to String to Make Subsequence.py

---
+++ problems/2486.Append Characters to String to Make Subsequence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        i = j = 0
+        while i< len(s) and j <len(t):
+            j += s[i]==t[j]
+            i += 1
+
+        return len(t) - j
