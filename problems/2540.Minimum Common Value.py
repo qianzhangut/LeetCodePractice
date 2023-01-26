b2426562c42da8557348e42dc5c744860710065f
qianzhangut <zhangqiandut@gmail.com>
2023-01-25 22:42:04 -0500

    2540.Minimum Common Value.py ver2

--- problems/2540.Minimum Common Value.py
+++ problems/2540.Minimum Common Value.py
@@ -1,3 +1,4 @@
+## two pointers
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
         p1,p2 = 0,0
@@ -11,3 +12,12 @@ class Solution:
                 p1+=1
 
         return -1
+        
+        
+        
+##use set 
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        com = set(nums1) & set(nums2)
+
+        return -1 if len(com) == 0 else min(com)
