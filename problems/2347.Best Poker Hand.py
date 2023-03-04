5f7ddfa752157de0c7ea897ad55e720bf8d890f1
qianzhangut <zhangqiandut@gmail.com>
2023-03-03 23:36:20 -0500

    "2347.Best Poker Hand.py" v2

--- problems/2347.Best Poker Hand.py
+++ problems/2347.Best Poker Hand.py
@@ -14,3 +14,24 @@ class Solution:
         elif max_ranks>=2:
             return "Pair"
         return ans
+        
+        
+### python match function
+class Solution:
+    def bestHand(self, ranks: List[int], suits: List[str]) -> str:
+  
+        d = {}
+        for i in ranks:
+            d[i] = d.get(i, 0) + 1
+
+        max_ranks = max(d.values())
+        if len(set(suits)) == 1:
+            return "Flush"
+
+        match max_ranks:
+            case 5 |  4 | 3:
+                return "Three of a Kind"
+            case 2:
+                return "Pair"
+            case _:
+                return "High Card"
