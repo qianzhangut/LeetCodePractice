adf6c84e77c4a480983b3c032b3f42dadc4e2804
qianzhangut <zhangqiandut@gmail.com>
2023-03-28 23:29:24 -0400

    2600.K Items With the Maximum Sum.py v2

--- problems/2600.K Items With the Maximum Sum.py
+++ problems/2600.K Items With the Maximum Sum.py
@@ -2,3 +2,8 @@ class Solution:
     def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
         ans = [1]*numOnes + [0]*numZeros + [-1]*numNegOnes
         return sum(ans[:k])
+        
+#one liner
+class Solution:
+    def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
+        return min(k, numOnes) - max(0, k - numZeros - numOnes)
