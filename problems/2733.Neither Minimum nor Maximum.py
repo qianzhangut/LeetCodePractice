81df2836f45e407055279c37d4300d77fe992cad
qianzhangut <zhangqiandut@gmail.com>
2023-06-12 23:37:32 -0400

    2733.Neither Minimum nor Maximum o1

--- problems/2733.Neither Minimum nor Maximum.py
+++ problems/2733.Neither Minimum nor Maximum.py
@@ -7,3 +7,17 @@ class Solution:
                 return i
 
         return -1
+        
+ #o(1) solution      
+class Solution:
+    def findNonMinOrMax(self, nums: List[int]) -> int:
+        if len(nums)<3: return -1
+
+        a, b = min(nums[0], nums[1]), max(nums[0], nums[1])
+        c = nums[2]
+
+        if (c<b and c>a): return c
+        if (c<a): return a
+        if (c>b): return b
+
+        reuturn -1
