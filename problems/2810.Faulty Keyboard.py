21f058f414a6e9279b5ff70745e5da9508d44635
qianzhangut <zhangqiandut@gmail.com>
2023-08-09 22:23:03 -0400

    / "2810.Faulty Keyboard.py"

---
+++ problems/2810.Faulty Keyboard.py
@@ -0,0 +1,12 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+        ans = ''
+
+        for c in s:
+            if c == 'i':
+                ans = ans[::-1]
+
+            else:
+                ans += c
+
+        return ans
