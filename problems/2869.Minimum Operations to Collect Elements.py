7d6a1f1aba4b0aded37361f9bf5a274ab4ecdb06
qianzhangut <zhangqiandut@gmail.com>
2023-10-05 22:54:54 -0400

    2869.Minimum Operations to Collect Elements.py v2

--- problems/2869.Minimum Operations to Collect Elements.py
+++ problems/2869.Minimum Operations to Collect Elements.py
@@ -4,3 +4,13 @@ class Solution:
         for i in range(len(nums)-1, -1, -1):
             if set(range(1,k+1)).issubset(set(nums[i:])):
                 return len(nums)-i
+                
+#set             
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        st  = set()
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i] <= k:
+                st.add(nums[i])
+            if len(st) == k:
+                return len(nums)-i
