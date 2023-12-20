18cf88456fa14b19bac2eb0f1d4b1d0f5aaa4867
qianzhangut <zhangqiandut@gmail.com>
2023-12-19 21:44:14 -0500

    2960.Count Tested Devices After Test Operations.py

--- problems/2960.Count Tested Devices After Test Operations.py
+++ problems/2960.Count Tested Devices After Test Operations.py
@@ -4,3 +4,17 @@ class Solution:
         for i in batteryPercentages:
             ans += i>ans
         return ans
+
+#brutal force
+class Solution:
+    def countTestedDevices(self, batteryPercentages: List[int]) -> int:
+        ans = 0
+        n = len(batteryPercentages)
+        for i in range(n):
+            if batteryPercentages[i] >0:
+                ans += 1
+                for j in range(i+1, n):
+                    batteryPercentages[j] = max(0, batteryPercentages[j] - 1)
+            
+
+        return ans
